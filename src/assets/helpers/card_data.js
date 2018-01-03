7cbcc4b79a85ef50214899c0fc885db611ef9a22
Scott Bowler <scott.bowler@learningfuze.com>
2018-01-03 14:58:15 -0800

    Initial setup complete

---
+++ src/assets/helpers/card_data.js
@@ -0,0 +1,26 @@
+import back from '../images/back.png';
+import react from '../images/react.svg';
+import angular from '../images/angular.png';
+
+export default [
+    {
+        back: back,
+        flipped: false,
+        front: react
+    },
+    {
+        back: back,
+        flipped: false,
+        front: react
+    },
+    {
+        back: back,
+        flipped: false,
+        front: angular
+    },
+    {
+        back: back,
+        flipped: false,
+        front: angular
+    }
+];
--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import cardData from '../assets/helpers/card_data';
 
-const App = () => (
-    <div className="app">
-        <h1>Memory Match</h1>
-    </div>
-);
+import Card from './card';
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            cards: cardData
+        }
+
+        this.flipCard = this.flipCard.bind(this);
+    }
+
+    flipCard(index){
+
+        const newCards = this.state.cards.slice();
+
+        newCards[index].flipped = !newCards[index].flipped;
+
+        this.setState({
+            cards: newCards
+        });
+    }
+
+
+    render(){
+
+        const cardElements = this.state.cards.map( (card, index ) => {
+            return <Card flip={() => this.flipCard(index)} key={index} card={card}/>
+        });
+
+        return (
+            <div className="app">
+                <h1>Memory Match</h1>
+                {cardElements}
+            </div>
+        );
+    }
+}
 
 export default App;
